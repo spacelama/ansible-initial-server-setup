c6e08562c56312d0f63cdc48863de24cf5bee138
Tim Connors <tconnors@rather.puzzling.org>
2023-05-03 23:13:49 +1000

    don't output errors on openwrt show_wifi_clients2.sh and upload them

--- roles/openwrt/files/bin/show_wifi_clients2.sh
+++ roles/openwrt/files/bin/show_wifi_clients2.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
- 
+
 # /etc/show_wifi_clients.sh
 # Shows MAC, IP address and any hostname info for all connected wifi devices
 # written for openwrt 12.09 Attitude Adjustment
 
 # https://openwrt.org/faq/how_to_get_a_list_of_connected_clients
- 
+
 echo    "# All connected wifi devices, with IP address,"
 echo    "# hostname (if available), and MAC address."
 echo -e "# IP address\tname\tMAC address"
@@ -13,16 +13,18 @@ echo -e "# IP address\tname\tMAC address"
 # (for MAC80211 driver; see wiki article for alternative commands)
 for interface in `iw dev | grep Interface | cut -f 2 -s -d" "`
 do
-  # for each interface, get mac addresses of connected stations/clients
-  maclist=`iw dev $interface station dump | grep Station | cut -f 2 -s -d" "`
-  # for each mac address in that list...
-  for mac in $maclist
-  do
-    # If a DHCP lease has been given out by dnsmasq,
-    # save it.
-    ip=`cat /proc/net/arp | awk '{print $1, $4}' | grep "$mac$" | awk '{print $1}'`
-    host=`nslookup "$ip" | sed -n 's/.*name = //p'`
-    # ... show the mac address:
-    echo -e "$ip\t$host\t$mac"
-  done
+    # for each interface, get mac addresses of connected stations/clients
+    maclist=`iw dev $interface station dump | grep Station | cut -f 2 -s -d" "`
+    # for each mac address in that list...
+    for mac in $maclist
+    do
+        # If a DHCP lease has been given out by dnsmasq,
+        # save it.
+        ip=`cat /proc/net/arp | awk '{print $1, $4}' | grep "$mac$" | awk '{print $1}'`
+        if [ -n "$ip" ] ; then
+            host=`nslookup "$ip" | sed -n 's/.*name = //p'`
+            # ... show the mac address:
+            echo -e "$ip\t$host\t$mac"
+        fi
+    done
 done
