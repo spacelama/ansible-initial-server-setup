c8d6514934f1ed80d7000d21b6eaa0ee7e06313d
Tim Connors <tconnors@rather.puzzling.org>
2023-06-04 22:58:31 +1000

    allow directories to override $BASH_FULL_HISTFILE via eg .envrc, and we start reading from and writing to that history instead

--- files/etc_bashrc.d/history.sh
+++ files/etc_bashrc.d/history.sh
@@ -2,7 +2,6 @@ function exitcleanly() {
     #    exiting=true
     # cancel all traps we set up ourselves to manage prompt and history:
     trap - 0 HUP DEBUG
-    #    writetohistory --full
     writetohistory "@$HOSTNAME CLOSE"
     exit $code
 }
@@ -14,14 +13,18 @@ function readfromhistory() {
 function writetohistory() {
     #FIXME: ideally detect when we're inside emacs TRAMP and do nothing
     #    bt
+    local BASH_FULLHIST="${BASH_FULL_HISTFILE:-$HOME/.bash_fullhistory}"
+    if [ "$BASH_FULL_HISTFILE_LAST" != "$BASH_FULLHIST" ] ; then
+        # time to read from the new file we're just setting, and start writing to it
+        BASH_FULL_HISTFILE_LAST="$BASH_FULLHIST"
+        setup_bash_history_file
+    fi
     history=$( history -a /dev/stdout ) # doesn't clear the history because in subshell
-    # echo "********history=$history"
     if [ -z "$1" -a -z "$history" ] ; then
         return
     fi
-    #    set -xv
     ( (
-        #    echo -n "writing history..."
+        # writing history...
         mkdir -p /tmp/$USER
         lockfile -1 -r 30 -l 60 /tmp/$USER/.bash_history.lock
         PWDENC="${PWD//!/\\!}"
@@ -34,31 +37,34 @@ function writetohistory() {
             fi | sed "/^#[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]$/s!\$! $PTS ${OPVIEW_VIEW:+$OPVIEW_VIEW/$OPVIEW_ITEM }$PWDENC$1!"
                )
         if [ -n "$history" ] ; then
-            echo "$history" >> $HOME/.bash_fullhistory
-            if [ -z "$1" ] ; then
-                #FIXME: I'm not sure anyone is using .bash_history anymore
-                echo "$history" >> $HOME/.bash_history
-            fi
+            echo "$history" >> "$BASH_FULLHIST"
         fi
-        #    if [ "$1" == --full ] ; then
-        #        command cp $HOME/.bash_fullhistory $HOME/.bash_history
-        #    fi
         command rm -f /tmp/$USER/.bash_history.lock
-        #    echo done
     ) & )
-    #    sleep 0.3 ; echo ; echo ; echo about to clear new history, sleeping a bit to clear race possible condition, about to dedup ; echo ; sleep 0.3
     if HISTTIMEFORMAT= history 2 | sed 's/^[ 0-9]*//' | uniq -d | grep -q . ; then
-        #        echo duplicated history
+        # duplicated history
         lasthist=$( history 1 | awk '{print $1}' )
-#        hiliteStdErr echo "DEBUG: we found a duplicated history line - we will try to delete lasthist=$lasthist" 1>&2
-#        history 10 | hiliteStdErr cat 1>&2
         echo "DEBUG: we found a duplicated history line - we will try to delete lasthist=$lasthist" 1>&2 >> /tmp/hist.$USER.dup
         history 10 | hiliteStdErr cat 1>&2 >> /tmp/hist.$USER.dup
         history -d $lasthist
     fi
     history -a /dev/null   # clear the history since the append is done in a subshell
-    #    set +xv
-    #    sleep 1 ; echo ; echo ; echo ; echo sleeping.5 ; sleep 0.5
+}
+
+function setup_bash_history_file() {
+    local BASH_FULLHIST="${BASH_FULL_HISTFILE:-$HOME/.bash_fullhistory}"
+
+    if [ -e "$BASH_FULLHIST" -a "$OS" = Linux ] ; then
+        #        echo reading history
+        HISTSIZE2=$((HISTSIZE*2)) # only an approximation since we
+                                  # have multiline history
+        tail -n $HISTSIZE2 "$BASH_FULLHIST" > /tmp/hist.$USER.$$
+        history -r /tmp/hist.$USER.$$
+        command rm -f /tmp/hist.$USER.$$
+    else
+        touch "$BASH_FULLHIST"
+        chmod 600 "$BASH_FULLHIST"
+    fi
 }
 
 function setup_bash_history_settings() {
@@ -78,15 +84,6 @@ function setup_bash_history_settings() {
     #    shopt -s cmdhist       #save all lines as one in history
     shopt -s lithist       #20170616 it seems that saving hist with timestamps allows us then to to read history back into a new session (even on bash3?) and still get all the literal newlines correctly.  So go back to lithist since it's so much better not having your history munged
     # see /etc/inputrc for show-all-if-ambiguous
-    if [ -e $HOME/.bash_fullhistory -a "$OS" = Linux ] ; then
-        #        echo reading history
-        HISTSIZE2=$((HISTSIZE*2)) # only an approximation since we
-                                  # have multiline history
-        tail -n $HISTSIZE2 $HOME/.bash_fullhistory > /tmp/hist.$USER.$$
-        history -r /tmp/hist.$USER.$$
-        command rm -f /tmp/hist.$USER.$$
-    else
-        touch $HOME/.bash_fullhistory
-        chmod 600 $HOME/.bash_fullhistory
-    fi
+
+    setup_bash_history_file
 }
