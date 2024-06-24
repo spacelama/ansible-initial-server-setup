3506cf20cfac1b332afbf8506f36721b1eefaa1e
Tim Connors <tconnors@rather.puzzling.org>
2024-06-24 22:31:06 +1000

    configure updatedb scan locations everywhere
    updatedb: allow ceph filesystems to be scanned from fs, and dirac to
    scan bind mounts; unify all other configs
    
    Justification:
    
    Now we're mounting systems on webserver, ceph, etc - obviously we need
    to have a master somewhere that does all the plocating (has to be
    mounted into the right place, so it can't be on pve*) with a
    much-reduced PRUNEFS and PRUNEPATHS, and we query it (the machine that
    mounts these, as readonly, could also be used for fs/backup). Or maybe
    we just allow ceph in plocate, and just do the plocate from everywhere
    that has access to it, knowing that we only mount things where we can
    (maybe have an explicit exclude on dirac-laptop), but all this
    requires removing autofs from excludes, which we don't want to rely on

---
+++ files/etc/updatedb.conf.fs
@@ -0,0 +1,6 @@
+PRUNE_BIND_MOUNTS="yes"
+# PRUNENAMES=".git .bzr .hg .svn"
+# NO GLOBS!
+PRUNENAMES="backuppc backuppc-clone .zfs"
+PRUNEPATHS="/tmp /var/spool /media /var/lib/os-prober /var/lib/ceph /home/.ecryptfs /var/lib/schroot /snapshots"
+PRUNEFS="NFS afs autofs binfmt_misc cgroup cgroup2 cifs coda configfs curlftpfs debugfs devfs devpts devtmpfs ecryptfs ftpfs fuse.cryfs fuse.encfs fuse.glusterfs fuse.gocryptfs fuse.gvfsd-fuse fuse.mfs fuse.rclone fuse.rozofs fuse.sshfs fusectl fusesmb hugetlbfs iso9660 lustre lustre_lite mfs mqueue ncpfs nfs nfs4 ocfs ocfs2 proc pstore rpc_pipefs securityfs shfs smbfs sysfs tmpfs tracefs udev udf usbfs"
