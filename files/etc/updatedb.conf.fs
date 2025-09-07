d5a8a336bce2b82c2fd825f51c0acbf9b053e16a
Tim Connors <tconnors@rather.puzzling.org>
2025-09-08 02:54:47 +1000

    we want all our ceph clients still indexing ceph with updatedb
    Half our ceph clients are containers, mounting their main work area
    off ceph.  They should still be able to index their main persistent
    storage with plocate/updatedb
    
    I search /var/spool likewise often enough

--- files/etc/updatedb.conf.fs
+++ files/etc/updatedb.conf.fs
@@ -2,6 +2,6 @@ PRUNE_BIND_MOUNTS="yes"
 # PRUNENAMES=".git .bzr .hg .svn"
 # NO GLOBS!
 PRUNENAMES="backuppc backuppc-clone .zfs"
-PRUNEPATHS="/tmp /var/spool /media /var/lib/os-prober /var/lib/ceph /home/.ecryptfs /var/lib/schroot /snapshots"
+PRUNEPATHS="/tmp /media /var/lib/os-prober /var/lib/ceph /home/.ecryptfs /var/lib/schroot /snapshots"
 # no autofs so ceph mounts can be indexed
 PRUNEFS="NFS afs binfmt_misc cgroup cgroup2 cifs coda configfs curlftpfs debugfs devfs devpts devtmpfs ecryptfs ftpfs fuse.cryfs fuse.encfs fuse.glusterfs fuse.gocryptfs fuse.gvfsd-fuse fuse.mfs fuse.rclone fuse.rozofs fuse.sshfs fusectl fusesmb hugetlbfs iso9660 lustre lustre_lite mfs mqueue ncpfs nfs nfs4 ocfs ocfs2 proc pstore rpc_pipefs securityfs shfs smbfs sysfs tracefs udev udf usbfs"
