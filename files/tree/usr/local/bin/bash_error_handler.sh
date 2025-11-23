a554a038fc87f89f291203155e773d0983b140f3
Tim Connors <tconnors@rather.puzzling.org>
2025-11-23 21:16:40 +1100

    bash_error_handler.sh: evaluate the failed line better
    In the interests of knowing which loop failed, attempt to
    perform variable substitution when getting bash to print
    out the failed line

--- files/tree/usr/local/bin/bash_error_handler.sh
+++ files/tree/usr/local/bin/bash_error_handler.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S echo "This should not be run directly - try using the following command, perhaps configuring \$ERROR_CONTINUE and \$ERROR_PREPEND: \nsource"
+#!/usr/bin/env -S echo "This should not be run directly - try using the following command, perhaps configuring \$ERROR_CONTINUE, \$ERROR_PRINT_UNSAFE: \nsource"
 
 # source this from your script that wants backtraces from your error
 # handler:
@@ -11,9 +11,15 @@
 shopt -so errtrace
 shopt -so nounset
 shopt -so pipefail
+if ! [ -t 2 ] ; then
+    # disable our STDERR colouring
+    function tput() {
+        :
+    }
+fi
 
 bold() {
-  tput bold; echo -n "$@"; tput sgr0
+    tput bold; echo -n "$@"; tput sgr0
 }
 
 print_context() {
@@ -22,38 +28,55 @@ print_context() {
 }
 
 ERR() {
-  local code=$?
-  local cmnd=$BASH_COMMAND
-  local func file line level
+    local code=$?
+    local cmnd=$BASH_COMMAND
+    local func file line level
 
-  echo "ERROR: $hostname:$0"
+    echo "ERROR: $hostname:$0"
 
-  echo "Exit status $(bold "$code") for command: $(bold "$cmnd")"
+    local nl=$'\n'
+    local FAILED_COMMAND=
+    if [ -n "${ERROR_PRINT_UNSAFE:-}" ] ; then
+        # Imagine you've failed on this line:
+        # openbracket='$(' ; closebracket=')' ; echo baaz & echo foo ; echo bar ; ls "$a $b" "|" "$c" `echo eee` $(echo fff) $( echo $a $b ; echo $b & echo $a ) $openbracket eval echo c d $closebracket `echo "$b $a"` > /tmp/bbbb.log
 
-  echo "${BASH_SOURCE[1]}:${BASH_LINENO[0]}:"
-  print_context "${BASH_SOURCE[1]}" "${BASH_LINENO[0]}"
+        # We want to eval it to expand all the variables which can
+        # appear in a number of different formats, but otherwise
+        # render all the shell special characters safe.  This is
+        # merely best efforts - don't use it on code you have no
+        # control over, takes user input, etc.  Merely a debugging aid
+        # to your own scripts.
 
-  for i in "${!FUNCNAME[@]}"; do
-    if [[ $i == 0 ]]; then continue; fi
-    func=${FUNCNAME[$i]}
-    file=${BASH_SOURCE[$i]}
-    line=${BASH_LINENO[$i - 1]}
-
-    if (( i == ${#FUNCNAME[@]} - 1 )); then
-      level='└─'
-      func="<$func>"
-    else
-      level='├─'
+        local SLIGHTLY_SAFENED_FAILED_COMMAND=$( echo "$cmnd" | sed 's/\([`()&;<>]\)/\\\1/g' )
+        FAILED_COMMAND="$nl>$(eval echo "$SLIGHTLY_SAFENED_FAILED_COMMAND")"
     fi
+    echo "Exit status $(bold "$code") for command:$nl $(bold "$cmnd$FAILED_COMMAND")"
 
-    echo " $level $(bold "$func") ($file:$line)"
-  done
+    echo "${BASH_SOURCE[1]}:${BASH_LINENO[0]}:"
+    print_context "${BASH_SOURCE[1]}" "${BASH_LINENO[0]}"
 
-  if [ -n "${ERROR_CONTINUE:-}" ] ; then
-      echo Continuuing...
-      exit=$code # but save the exit code into a variable the caller can use
-  else
-      exit $code
-  fi
+    for i in "${!FUNCNAME[@]}"; do
+        if [[ $i == 0 ]]; then continue; fi
+        local func=${FUNCNAME[$i]}
+        local file=${BASH_SOURCE[$i]}
+        local line=${BASH_LINENO[$i - 1]}
+
+        if (( i == ${#FUNCNAME[@]} - 1 )); then
+            local level='└─'
+            func="<$func>"
+        else
+            local level='├─'
+        fi
+
+        echo " $level $(bold "$func") ($file:$line)"
+    done
+
+    if [ -n "${ERROR_CONTINUE:-}" ] ; then
+        echo Continuing...
+
+        exit=$code # but save the exit code into a variable the caller can use
+    else
+        exit $code
+    fi
 } >&2
 trap ERR ERR
